ab7cfc1054a6fb8823119b65d368d31be99dd229
jrudascas <jrudascas@gmail.com>
2017-02-06 15:58:36 -0500

    PyTP

---
+++ PyTP/src/encapsulamiento/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package encapsulamiento;
+
+public class Test {
+    public int a = 1;
+    private int b = 2;
+    protected int c = 3;
+    int d = 4;
+}
